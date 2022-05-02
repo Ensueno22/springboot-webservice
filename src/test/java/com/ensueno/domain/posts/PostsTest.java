534e3d3ead0021cb2a7c571984649c97cef5195b
Ensueno22 <seongkwon87@gmail.com>
2022-05-02 19:39:37 +0900

    MVC 기본 코드 commit;

---
+++ src/test/java/com/ensueno/domain/posts/PostsTest.java
@@ -0,0 +1,43 @@
+package com.ensueno.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+
+@SpringBootTest
+class PostsTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("seongkwon87@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertEquals(posts.getTitle(), "테스트 게시글");
+        assertEquals(posts.getContent(), "테스트 본문");
+    }
+
+}
